fd8f5b4ce022eb1c8f100738f7e0ef2c73c16192
Homood <humoud.alsaleh@kgl.com>
2016-12-05 11:13:30 +0300

    reading twitter tkns and creds from yml file.

--- config/boot.rb
+++ config/boot.rb
@@ -31,17 +31,20 @@ end
 # DeveloperAccount1!
 
 # Authenticate
+twitter_creds = YAML.load_file('config/twitter.yml')
+BOT_HANDLER = twitter_creds['bot_handler']
+
 CLIENT = Twitter::REST::Client.new do |config|
-  config.consumer_key = 'svNAuwr8sRcfc49z3mOijgI9N'
-  config.consumer_secret = 'OCI9eMFwlSAojr8QDgc8xHFdfOL1ffLkANVQMGugKuYcRH27t0'
-  config.access_token = '3018717885-QbTctEiLYQcQBRqOe51Ef6DZbBxmTSg3i6FwZCg'
-  config.access_token_secret = 'a0rcKEezIgbOMO5ITKiT9M1IGmX4xvS6JMJbUWaoidGZD'
+  config.consumer_key = twitter_creds['consumer_key']
+  config.consumer_secret = twitter_creds['consumer_secret']
+  config.access_token = twitter_creds['access_token']
+  config.access_token_secret = twitter_creds['access_token_secret']
 end
 
 S_CLIENT = TweetStream.configure do |config|
-  config.consumer_key = 'svNAuwr8sRcfc49z3mOijgI9N'
-  config.consumer_secret = 'OCI9eMFwlSAojr8QDgc8xHFdfOL1ffLkANVQMGugKuYcRH27t0'
-  config.oauth_token        = '3018717885-QbTctEiLYQcQBRqOe51Ef6DZbBxmTSg3i6FwZCg'
-  config.oauth_token_secret = 'a0rcKEezIgbOMO5ITKiT9M1IGmX4xvS6JMJbUWaoidGZD'
+  config.consumer_key = twitter_creds['consumer_key']
+  config.consumer_secret = twitter_creds['consumer_secret']
+  config.oauth_token        = twitter_creds['access_token']
+  config.oauth_token_secret = twitter_creds['access_token_secret']
   config.auth_method        = :oauth
 end
--- twitter-music-bot.rb
+++ twitter-music-bot.rb
@@ -2,8 +2,8 @@ require File.join(__dir__, 'config/boot')
 
 
 puts "app is up and running...\n"
-
-TweetStream::Client.new.track("@RobotPawn") do |tweet|
+puts "tracking bot with handler: #{BOT_HANDLER}..."
+TweetStream::Client.new.track(BOT_HANDLER) do |tweet|
   Bot.save_tweet(tweet)
   puts "Saved a tweet:\t#{tweet.full_text}."
   puts "Issue job that replies to tweet..."
