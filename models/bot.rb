fb7189d63bebec457c692ed0051e00d53b8591a8
Homood <humoud.alsaleh@kgl.com>
2016-12-06 11:03:12 +0300

    searching for songs based on lyrics.

--- models/bot.rb
+++ models/bot.rb
@@ -20,41 +20,74 @@ class Bot < ActiveRecord::Base
     t.save
   end
 
-  def self.reply_to_mention(handle, msg, tweet_id)
-    CLIENT.update("@#{handle} #{msg}", in_reply_to_status_id: tweet_id)
+  # Expects:
+  # First line of tweet = Song name
+  # Second line of tweet = Artist name
+  def self.reply_to_mention_direct_search(tweet)
+    # Remove bot handle from tweet
+    tweet_txt = tweet.text.sub(/^@\w*\s/, "")
+    # Get artist and song from tweet
+    artist_txt = tweet_txt.split("\n")[1].gsub(" ", "%20")
+    song_txt = tweet_txt.split("\n")[0].gsub(" ", "%20")
+
+    # Prepare URL for request http
+    url_str = "http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?"
+    url = URI("#{url_str}artist=#{artist_txt}&song=#{song_txt}")
+    # Make http request
+    response = make_http_request(url)
+    response_hash = make_hash(response.read_body)
+
+    puts "====\n\nHash: #{response_hash["GetLyricResult"]}\n\n====\n\n"
+
+    if response_hash["GetLyricResult"]["Lyric"].blank?
+      CLIENT.update("@#{tweet.user.screen_name} Sorry :(\ncouldn't find the lyrics", in_reply_to_status_id: tweet.id)
+    else
+      lyrics = nil
+      if response_hash["GetLyricResult"]["Lyric"].length > 120
+        lyrics = response_hash["GetLyricResult"]["Lyric"][0..120]
+      else
+        lyrics = response_hash["GetLyricResult"]["Lyric"]
+      end
+      CLIENT.update("@#{tweet.user.screen_name} #{lyrics}", in_reply_to_status_id: tweet.id)
+    end
   end
 
-  # def self.search_twitter_for_words(words)
-  #   CLIENT.search(words, lang: "en").first.text
-  # end
-  #
-  # def self.check_mentions
-  #   puts "Reading mentions..."
-  #   CLIENT.mentions_timeline.each { |tweet|
-  #     # if did_reply_to_mention? tweet.id
-  #       # puts "replied to tweet with id: #{tweet.id}"
-  #     # else
-  #       print_tweet tweet
-  #     # end
-  #   }
-  # end
-  #
-  # def self.mention(name, msg)
-  #   CLIENT.update("@#{name} #{mg}")
-  # end
-  #
-  # def self.reply_to_tweet(name, msg, tweet_id)
-  #   CLIENT.update("@#{name} msg", in_reply_to_status_id: tweet_id)
-  # end
-  #
-  # # def self.did_reply_to_mention?(tweet_id)
-  # #   if (Tweet.find_by(tweet_id: tweet_id).nil?)
-  # #     return false
-  # #   else
-  # #     return true
-  # #   end
-  # # end
+  # Expects:
+  # the whole tweet contains just lyrics
+  # and of course the first part of the tweet is the bot twitter handler
+  # just removed in the first line in the method  using regex
+  def self.reply_to_mention_search_lyric(tweet)
+    # Remove bot handle from tweet
+    tweet_txt = tweet.text.sub(/@\w*\s*/, "")
 
+    url_str = "http://api.chartlyrics.com/apiv1.asmx/SearchLyricText?"
+    url = URI("#{url_str}lyricText=#{tweet_txt}")
+    res = make_http_request(url)
+    res_hash = make_hash(res.read_body)
+
+    # if nothing found
+    if res_hash["ArrayOfSearchLyricResult"]["SearchLyricResult"].count == 1
+      CLIENT.update("@#{tweet.user.screen_name} Sorry :(\ncouldn't find the song name and artist", in_reply_to_status_id: tweet.id)
+    else
+      reply_arr = []
+      temp_txt = ""
+      res_hash["ArrayOfSearchLyricResult"]["SearchLyricResult"].each { |entry|
+        if entry.nil?
+          reply_arr.push(temp_txt)
+          next
+        end
+        if temp_txt.length > 60
+          reply_arr.push(temp_txt)
+          temp_txt = "#{entry["Song"]} - #{entry["Artist"]}"
+        else
+          temp_txt = "#{temp_txt}\n#{entry["Song"]} - #{entry["Artist"]}"
+        end
+      }
+      reply_arr.each { |tweet_msg|
+        CLIENT.update("@#{tweet.user.screen_name} #{tweet_msg}", in_reply_to_status_id: tweet.id)
+      }
+    end
+  end
 
   private
     # removes html entities from tweet text then prints it out
@@ -65,5 +98,18 @@ class Bot < ActiveRecord::Base
       else
         puts "#{tweet.full_text.to_s}\n"
       end
+    end
+
+    def self.make_hash(response_body)
+      doc = Nokogiri::XML(response_body)
+      response_hash = Hash.from_xml(doc.to_s)
+      return response_hash
+    end
+
+    def self.make_http_request(url)
+      http = Net::HTTP.new(url.host, url.port)
+      request = Net::HTTP::Get.new(url)
+      response = http.request(request)
+      return response
     end
 end
--- services/reply_to_mention_job.rb
+++ services/reply_to_mention_job.rb
@@ -2,35 +2,6 @@ class ReplyToMentionJob
   include SuckerPunch::Job
 
   def perform(tweet)
-    tweet_txt = tweet.text.sub(/^@\w*\s/, "")
-
-    artist_txt = tweet_txt.split("\n")[1].gsub(" ", "%20")
-    song_txt = tweet_txt.split("\n")[0].gsub(" ", "%20")
-
-    url_str = "http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?"
-    url = URI("#{url_str}artist=#{artist_txt}&song=#{song_txt}")
-
-    http = Net::HTTP.new(url.host, url.port)
-
-    request = Net::HTTP::Get.new(url)
-
-    response = http.request(request)
-
-    doc = Nokogiri::XML(response.read_body)
-    response_hash = Hash.from_xml(doc.to_s)
-
-    puts "====\n\nHash: #{response_hash["GetLyricResult"]}\n\n====\n\n"
-
-    if response_hash["GetLyricResult"]["Lyric"].blank?
-      CLIENT.update("@#{tweet.user.screen_name} Sorry :(\ncouldn't find the lyrics", in_reply_to_status_id: tweet.id)
-    else
-      lyrics = nil
-      if response_hash["GetLyricResult"]["Lyric"].length > 120
-        lyrics = response_hash["GetLyricResult"]["Lyric"][0..120]
-      else
-        lyrics = response_hash["GetLyricResult"]["Lyric"]
-      end
-      CLIENT.update("@#{tweet.user.screen_name} #{lyrics}", in_reply_to_status_id: tweet.id)
-    end
+    Bot.reply_to_mention_search_lyric(tweet)
   end
 end
